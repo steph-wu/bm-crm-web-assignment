42b24c7e1a7e3d5447889f6e061cac8b0c5e9f5a
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-24 14:23:04 -0500

    Post add contact form and redirect to contacts page

--- crm.rb
+++ crm.rb
@@ -1,11 +1,6 @@
 require_relative 'contact'
 require 'sinatra'
 
-# Fake contacts
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
-
 get '/' do
   @crm_app_name = "Stephanie's CRM"
   erb :index
@@ -17,4 +12,9 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
