ce3ea80aea3b8c9d40e7a8b2e5a147958c8d9b13
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-25 14:36:50 -0500

    Edit contact form and submission

--- crm.rb
+++ crm.rb
@@ -36,8 +36,21 @@ get '/contacts/:id/edit' do
   end
 end
 
-
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
