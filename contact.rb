736c2b3d1de10447a7c33b99b7473dbb1f2bc4ff
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-26 12:28:06 -0500

    Set up active record, mini record and sql

--- contact.rb
+++ contact.rb
@@ -1,3 +1,10 @@
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
+
+
 class Contact
   attr_accessor :first_name, :last_name, :email, :note
   attr_reader :id
